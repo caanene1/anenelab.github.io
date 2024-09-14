22cf766599b3d2f781c76231aa63a83878a8f4d2
Chinedu Anene <caanenedr@outlook.com>
2024-09-14 20:37:17 +0100

    settings
    settings update

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `BiOzi | Bioinformatics and data analytics services`,
-	description: `Custom bioinformatics, biostatistics and ML/AI services to accelerate your research.`,
-	url: `https://biozi.co.uk`, // No trailing slash!
-	name: `BiOzi`, // Used for things like the copyright in the footer.
+	title: `Anene Lab `,
+	description: `Bioinformatics and Genomics`,
+	url: `https://anenelab.github.io`, // No trailing slash!
+	name: `anenelab`, // Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
   showLittleSticksPlug: false, // Disable to remove the Little Sticks plug from the footer.
 };
